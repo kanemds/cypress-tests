bf8e106e0f430d06876963fac18d540a44abae58
kanemds <kanemdeng@gmail.com>
2024-02-19 13:39:13 -0800

    base url

--- cypress/e2e/k-blog.cy.js
+++ cypress/e2e/k-blog.cy.js
@@ -1,5 +1,14 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  it('contain correct navbar hearder text', () => {
+    cy.visit('/')
+    cy.get("[data-test='k-blog-header']").contains(/K-BLOG/i)
+    cy.get("[data-test='k-blog-header']").should('contain.text', 'K-BLOG')
+    cy.get("[data-test='k-blog-header']").should($div => {
+      console.log($div.text())
+      let text = $div.text().toUpperCase()
+      let expectedText = 'k-blog'.toUpperCase()
+      console.log(expectedText)
+      expect(text).to.equal(expectedText)
+    })
   })
 })
